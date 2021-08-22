057796ba8308107fd772e27b5ab074f9bc4cea7b
Akerlin <56051184+Angop@users.noreply.github.com>
2021-08-22 13:10:36 -0700

    added testcase file

---
+++ test_ko.py
@@ -0,0 +1,12 @@
+# Test cases for knowop assignment
+# Angela Kerlin
+# Marcelo 
+
+import unittest
+from knowop import *
+
+
+class TestBoardStupid(unittest.TestCase):
+    
+    def test_(self):
+        pass
